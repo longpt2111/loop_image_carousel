c964b87c2995af252b5ba65894ce0c7d7bc9b7c7
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2023-03-10 11:04:08 +0700

    commit changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,47 @@
-const imageWrapper = document.querySelector(".image-wrapper");
-const imageItems = document.querySelectorAll(".image-wrapper > *");
-const imageLength = imageItems.length;
-const perView = 3;
-const delay = 2000;
-let scrollCounter = 0;
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-imageWrapper.style.setProperty("--per-view", perView);
-
-for (let i = 0; i < perView; i++) {
-  imageWrapper.insertAdjacentHTML("beforeend", imageItems[i].outerHTML);
-}
-
-let autoScroll = setInterval(scrolling, delay);
-
-function scrolling() {
-  scrollCounter++;
-  if (scrollCounter === imageLength + 1) {
-    clearInterval(autoScroll);
-    scrollCounter = 1;
-    imageWrapper.style.left = "0";
-    imageWrapper.style.transition = "0s";
-    autoScroll = setInterval(scrolling, delay);
+class Carousel {
+  constructor() {
+    this.imageWrapper = $(".image-wrapper");
+    this.imageItems = $$(".image-wrapper > *");
+    this.imageLength = this.imageItems.length;
+    this.scrollCounter = 0;
   }
-  const imageWidth =
-    document.querySelector(".image-wrapper > :first-child").offsetWidth + 24;
-  imageWrapper.style.left = `-${scrollCounter * imageWidth}px`;
-  imageWrapper.style.transition = "0.3s";
+
+  setImagesPerView(n) {
+    this.imageWrapper.style.setProperty("--images-per-view", n);
+    this.addCloneItems(n);
+  }
+
+  addCloneItems(n) {
+    for (let i = 0; i < n; i++) {
+      this.imageWrapper.insertAdjacentHTML(
+        "beforeend",
+        this.imageItems[i].outerHTML
+      );
+    }
+  }
+
+  handleAutoScroll(delay) {
+    this.autoScroll = setInterval(() => this.handleScrolling(delay), delay);
+  }
+
+  handleScrolling(delay) {
+    this.scrollCounter++;
+    if (this.scrollCounter === this.imageLength + 1) {
+      clearInterval(this.autoScroll);
+      this.scrollCounter = 1;
+      this.imageWrapper.style.left = "0";
+      this.imageWrapper.style.transition = "0s";
+      this.handleAutoScroll(delay);
+    }
+    this.imageWidth = $(".image-wrapper > :first-child").offsetWidth + 24;
+    this.imageWrapper.style.left = `-${this.scrollCounter * this.imageWidth}px`;
+    this.imageWrapper.style.transition = "0.3s";
+  }
 }
+
+const carousel = new Carousel();
+carousel.setImagesPerView(3);
+carousel.handleAutoScroll(2000);
